e805d46a322f86e5e538f1edfe5139f497612f38
CP <chaezen@gmail.com>
2025-06-21 14:26:15 -1000

    Updated Script

--- MakahikiGames/Assets/Scripts/CameraSwitch.cs
+++ MakahikiGames/Assets/Scripts/CameraSwitch.cs
@@ -2,34 +2,43 @@ using UnityEngine;
 
 public class CameraSwitch : MonoBehaviour
 {
-    public GameObject Camera1; 
-    public GameObject Camera2;
+    public Camera[] cameras; 
+
     bool cam1, cam2;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Camera1.SetActive(true);
+        // if (cameras.Length > 1)
+        // {
+        //     for (int i = 1; i < cameras.Length; i++)
+        //     {
+        //         cameras[i].gameObject.SetActive(false);
+        //     }
+        // }
+
+        cameras[0].gameObject.SetActive(true);
         cam1 = true;
-        Camera2.SetActive(false);
+        cameras[1].gameObject.SetActive(false);
         cam2 = false;
     }
+    // if (Input.GetKeyDown(KeyCode.JoystickButton4))
 
     // Update is called once per frame
     void Update()
     {
         if (cam1 && Input.GetKeyDown(KeyCode.Tab))
         {
-            Camera1.SetActive(false);
+            cameras[0].gameObject.SetActive(false);
             cam1 = false;
-            Camera2.SetActive(true);
+            cameras[1].gameObject.SetActive(true);
             cam2 = true;
         }
         else if (cam2 && Input.GetKeyDown(KeyCode.Tab))
         {
-            Camera1.SetActive(true);
+            cameras[0].gameObject.SetActive(true);
             cam1 = true;
-            Camera2.SetActive(false);
+            cameras[1].gameObject.SetActive(false);
             cam2 = false;
         }
     }
