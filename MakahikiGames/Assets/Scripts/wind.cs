bb588f41985610a4e591557367090f9c3c34ee0a
CP <chaezen@gmail.com>
2025-06-26 14:38:25 -1000

    Organized

---
+++ MakahikiGames/Assets/Scripts/wind.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class wind : MonoBehaviour
+{
+    public float windSpeed = 0f;
+    public bool isLeft;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        isLeft = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
