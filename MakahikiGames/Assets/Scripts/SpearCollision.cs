355b73fd3034bcb9360298ee3a48fecf188aae36
CP <chaezen@gmail.com>
2025-07-15 14:41:52 -1000

    TestLevel with arc preview, cam aim zoom, tweaked points
    arc doesnt work 3D
    - maybe have a dot in middle
    - zoom needs UI?
    
    point system needs to be reworked

--- MakahikiGames/Assets/Scripts/SpearCollision.cs
+++ MakahikiGames/Assets/Scripts/SpearCollision.cs
@@ -8,7 +8,9 @@ public class SpearCollision : MonoBehaviour
     private Rigidbody rb;
     public CameraSwitch CameraSwitch;
     public ScoreSystem scoreSystem;
-    
+    public GameObject LinePoint;
+    public Vector3 TheLine;
+
     private bool onGround;
     private float timer = 0.0f;
     private int seconds = 0;
@@ -16,7 +18,7 @@ public class SpearCollision : MonoBehaviour
     public bool timerOn;
     public int waitime = 5;
     public int savedWait;
-    
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -31,6 +33,7 @@ public class SpearCollision : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        TheLine = LinePoint.transform.position;
         if (onGround || timerOn)
         {
             timer += Time.deltaTime;
@@ -56,7 +59,7 @@ public class SpearCollision : MonoBehaviour
             rb.rotation = Quaternion.LookRotation(rb.linearVelocity) * Quaternion.Euler(89, 0, 0);
         }
         //Math Way with center of mass
-       // rb.AddForceAtPosition(rb.linearVelocity * -0.1f, transform.TransformPoint(0,-.5f,0));   
+        // rb.AddForceAtPosition(rb.linearVelocity * -0.1f, transform.TransformPoint(0,-.5f,0));   
     }
 
     private void OnTriggerEnter(Collider collision)
@@ -68,13 +71,13 @@ public class SpearCollision : MonoBehaviour
             {
                 rb.constraints = RigidbodyConstraints.FreezeAll;
                 timerOn = true;
-                
-            Vector3 impactPoint = spearHead.position;
-            Debug.Log("Impact Point: " + impactPoint);
+
+                Vector3 impactPoint = spearHead.position;
+                Debug.Log("Impact Point: " + impactPoint);
 
 
-            // Calculate distance from impact point to center of the target
-            scoreSystem.Hit(impactPoint);
+                // Calculate distance from impact point to center of the target
+                scoreSystem.Hit(impactPoint);
             }
         }
 
@@ -112,5 +115,10 @@ public class SpearCollision : MonoBehaviour
                 CameraSwitch.SpearCamSwitch();
             }
         }
+    }
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawLine(transform.position, TheLine);
     }
 }
--- MakahikiGames/Assets/Scripts/ThrowSpear.cs
+++ MakahikiGames/Assets/Scripts/ThrowSpear.cs
@@ -16,6 +16,7 @@ public class ThrowSpear : MonoBehaviour
     public Transform spearPos;
     public LayerMask tree;
     public Camera Maincam;
+    public ArcPreview drawArc;
 
     public float strength = 0f;
     public float strengthMult = 4f;
@@ -89,6 +90,7 @@ public class ThrowSpear : MonoBehaviour
                         spearCollision.isThrown = isThrown;
                         ammoRemaining--;
                         uiManager.ammoRemaining(ammoRemaining);
+                        drawArc.isThrown = isThrown;
 
                     }
 
@@ -119,7 +121,7 @@ public class ThrowSpear : MonoBehaviour
             // rb.AddForce(aimDir.normalized * strength, ForceMode.Impulse);
 
             //Camera forward aim
-            rb.AddForce(Maincam.transform.forward * strength, ForceMode.Impulse);
+            rb.AddForce(spear.transform.up * strength, ForceMode.Impulse);
 
             //For Spawn in version
             
@@ -136,6 +138,7 @@ public class ThrowSpear : MonoBehaviour
         {
             strength = timer * strengthMult;
             SpearUI.SpearCharge(strength);
+            drawArc.launchForce = strength;
         }
         if (strength >= maxCharge)
         {
@@ -161,6 +164,8 @@ public class ThrowSpear : MonoBehaviour
             rb.constraints = RigidbodyConstraints.FreezeAll;
             spear.transform.rotation = Quaternion.Euler(new Vector3(90, 0, 0));
             isThrown = false;
+            spearCollision.isThrown = false;
+            drawArc.isThrown = isThrown;
             Debug.Log("resetSpear");
         }
             if (ammoRemaining == 0 && !isPracticeMode)
