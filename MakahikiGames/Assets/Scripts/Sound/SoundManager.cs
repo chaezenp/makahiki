6a192a080e7efe76b0ee4dd9932c5a4ede88607a
CP <chaezen@gmail.com>
2025-08-13 10:40:56 -1000

    SpearSounds + SoundManager

---
+++ MakahikiGames/Assets/Scripts/Sound/SoundManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public enum SoundType
+{
+    SPEARTHROW,
+    SPEARWIND,
+    SPEARHIT,
+    BACKGROUND
+}
+
+[RequireComponent(typeof(AudioSource))]
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] soundList;
+    private static SoundManager instance;
+    private AudioSource audioSource;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();   
+    }
+    public static void PlaySound(SoundType sound, float volume = 1)
+    {
+        instance.audioSource.PlayOneShot(instance.soundList[(int)sound], volume);
+    }
+}
